4951962c626ac31f4edb27463af7bccd73106915
philcallister <philcallister@gmail.com>
2016-05-23 21:34:24 -0500

    Updated command/socket processing

--- lib/elixir_fake_im/command.ex
+++ lib/elixir_fake_im/command.ex
@@ -5,13 +5,17 @@ alias ElixirFakeIm.MQ
 
 defmodule ElixirFakeIm.Command do
 
+  ##
+  # Login
+  # > login:<user>
   def parse_login(line) do
-    case String.split(line, ":", trim: true) do
-      ["login", user] -> {:ok, String.strip(user)}
+    case String.split(line, ":", trim: true) |> Enum.map(&String.strip(&1)) do
+      ["login", user] when byte_size(user) > 0 -> {:ok, user}
       _ -> {:error, :login_required}
     end
   end
 
+  ## login:<user>
   def run_login(socket, {:ok, user}) do
     {:ok, mq_pid} = UserPool.create(UserPool, user)
     MQ.put_socket(mq_pid, socket)
@@ -19,17 +23,31 @@ defmodule ElixirFakeIm.Command do
     {:ok, user}
   end
 
+  ##
+  # Commands
+  # > logout
+  # > user:<user>:<msg>
   def parse(line) do
-    case String.split(line, ":", trim: true) do
-      ["user", to_user, msg] -> {:ok, {:user, String.strip(to_user), msg}}
-      _ -> {:error, :unknown_command}
+    case String.split(line, ":", trim: true) |> Enum.map(&String.strip(&1)) do
+      ["logout"] -> {:ok, :logout}
+      ["user", to_user, msg]  when byte_size(to_user) > 0 -> {:ok, {:user, to_user, msg}}
+      _ -> {:error, :msg, "UNKNOWN COMMAND"}
     end
   end
 
-  def run(_socket, {:user, to_user, msg}) do
-    {:ok, mq_pid} = UserPool.lookup(UserPool, to_user)
-    to_socket = MQ.get_socket(mq_pid)
-    {:ok, msg, [{to_user, to_socket}]}
+  def run(_socket, from_user, :logout) do
+    Logger.info("Logout: #{from_user}")
+    {:error, :closed}
+  end
+
+  def run(_socket, _from_user, {:user, to_user, msg}) do
+    case UserPool.lookup(UserPool, to_user) do
+      {:ok, mq_pid} ->
+        to_socket = MQ.get_socket(mq_pid)
+        {:ok, msg, [{to_user, to_socket}]}
+      :error ->
+        {:error, :msg, "[#{to_user}] USER DISCONNECTED"}
+    end
   end
 
 end
--- lib/elixir_fake_im/socket_connection.ex
+++ lib/elixir_fake_im/socket_connection.ex
@@ -3,88 +3,45 @@ require Logger
 defmodule ElixirFakeIm.SocketConnection do
 
   alias ElixirFakeIm.Command
-  alias ElixirFakeIm.UserPool
+  alias ElixirFakeIm.SIO
 
   ##
-  # Wait for login here...
+  # Loop for login here...
   def login(socket) do
     msg = 
-      case read_line(socket) do
+      case SIO.read_line(socket) do
         {:ok, data} ->
           case Command.parse_login(data) do
-            {:ok, _user} = command ->
-              Command.run_login(socket, command)
-            {:error, _} = error ->
-              error
+            {:ok, _user} = command -> Command.run_login(socket, command)
+            {:error, _} = error -> error
           end
-        {:error, _} = error ->
-          error
+        {:error, _} = error -> error
       end
 
-    write_line(socket, msg)
+    SIO.write_line(socket, msg)
 
     case msg do
-      {:error, _} -> 
-        login(socket)
-      _ ->
-        serve(socket, msg)
+      {:error, _} -> login(socket)
+      _ -> serve(socket, msg)
     end
 
   end
 
-  defp serve(socket, {:ok, from_name}) do
+  ##
+  # Loop for commands here...
+  defp serve(socket, {:ok, from_user}) do
     msg = 
-      case read_line(socket) do
+      case SIO.read_line(socket) do
         {:ok, data} ->
           case Command.parse(data) do
-            {:ok, command} ->
-              Command.run(socket, command)
-            {:error, _} = error ->
-              error
-          end
+            {:ok, command} -> Command.run(socket, from_user, command)
+            {:error, _} = error -> error
+            {:error, _, _} = error -> error
+         end
         {:error, _} = error -> error
       end
-    write_line(socket, from_name, msg)
-    serve(socket, {:ok, from_name})
-  end
-
-  defp read_line(socket) do
-    :gen_tcp.recv(socket, 0)
-  end
-
-  defp write_line(socket, {:ok, name}) do
-    :gen_tcp.send(socket, "Sending [#{name}]: LOGGED IN\n")
-  end
-
-  defp write_line(socket, {:error, :login_required}) do
-    :gen_tcp.send(socket, "LOGIN REQUIRED\n")
-  end
-
-  defp write_line(socket, name, {:ok, msg, [{_to_user, to_socket}|users]}) do
-    :gen_tcp.send(to_socket, "Sending [#{name}]: #{msg}\n")
-    write_line(socket, name, {:ok, msg, users})
-  end
-  defp write_line(_socket, _name, {:ok, _msg, []}), do: []
-
-
-  defp write_line(socket, _name, {:error, :unknown_command}) do
-    :gen_tcp.send(socket, "UNKNOWN COMMAND\n")
-  end
-
-  defp write_line(_socket, name, {:error, :closed}) do
-    mq_shutdown(name)
-    exit(:shutdown)
-  end
-
-  defp write_line(socket, name, {:error, error}) do
-    mq_shutdown(name)
-    :gen_tcp.send(socket, "ERROR\n")
-    exit(error)
-  end
-
-  defp mq_shutdown(name) do
-    {:ok, mq_pid} = UserPool.lookup(UserPool, name)
-    Process.exit(mq_pid, :shutdown)
+    SIO.write_line(socket, from_user, msg)
+    serve(socket, {:ok, from_user})
   end
 
 end
